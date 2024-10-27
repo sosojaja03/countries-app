8b92e44eae48b26d8e9608c20bd330402d10e74a
sosojaja03 <sosojajanidze486@gmail.com>
2024-10-27 17:22:45 +0400

    finaly fic for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-    // const rootPath = path.resolve(process.cwd());
-    // const srcPath = `${rootPath}/src`;
-    return {
-        plugins: [react()],
-        resolve: {
-            // alias: {
-            //   "~": srcPath,
-            //   "@": rootPath,
-            // },
-            alias: { '@': path.resolve(__dirname, './src') },
-        },
-    };
+  // const rootPath = path.resolve(process.cwd());
+  // const srcPath = `${rootPath}/src`;
+  return {
+    plugins: [react()],
+    resolve: {
+      // alias: {
+      //   "~": srcPath,
+      //   "@": rootPath,
+      // },
+      alias: { '@': path.resolve(__dirname, './src') },
+    },
+  };
 });
